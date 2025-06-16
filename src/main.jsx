acc29f55f925d9a7ae8bd16513e4db276f4ccf02
Adrián Martín Romo Cañadas <adrianmrc94@gmail.com>
2025-06-16 10:17:35 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
-import { StoreProvider } from './hooks/useGlobalReducer'; // Importa el Provider
+import { StoreProvider } from './hooks/useGlobalReducer';
 import { ToastContainer } from 'react-toastify';
 
 
